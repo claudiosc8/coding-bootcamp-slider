adaf8e911118a68d8d253488c1d5e283b5fc7bbd
Claudio <claudiosdv@gmail.com>
2020-07-12 19:47:46 +0100

    full working version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,97 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "@popmotion/popcorn";
+import iconNext from './images/icon-next.svg'
+import iconPrev from './images/icon-prev.svg'
+import Items from './Items'
 import './App.scss';
+
+
+
 
 function App() {
 
-  const [showMenu, setShowMenu] = useState(false)
+  const [page, setPage] = useState(0);
+  const [direction, setDirection] = useState(0);
+  const [update, setUpdate] = useState(false);
+
+  const imageIndex = wrap(0, Items.length, page);
+
+  const handlePage = newDirection => {
+      setDirection(newDirection)
+      setUpdate(!update)
+  }
+
+
+  const handleKey = e => {
+    if (e.keyCode === 37) {
+      handlePage(-1)
+    } else if (e.keyCode === 39) {
+      handlePage(1)
+    }
+  }
+
+  useEffect(()=> {
+    setPage(p => p + direction)
+  }, [direction, update])
 
   return (
       
-      <React.Fragment>
-     
-      </React.Fragment>
+      <div className='container'>
+      <div className='testimonial' onKeyDown={handleKey} tabIndex="0">
+
+
+          <div 
+          className='text-wrapper' 
+          >
+            <div className='text'>
+            <motion.span
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{opacity: { duration: 1 }}}
+            key={page}
+            >
+            {Items[imageIndex].text}
+            </motion.span>
+            </div>
+            <motion.div 
+              className='info'
+              initial={{opacity:0}}
+              animate={{opacity:1}}
+              transition={{opacity: { duration: 1 }}}
+              key={page}
+            >
+              <div className='source'>{Items[imageIndex].source}</div>
+              <div className='role'>{Items[imageIndex].role}</div>
+            </motion.div>
+          </div>
+
+  
+        <div className='image-wrapper'>
+          <div className='controls'> 
+            <span onClick={() => handlePage(-1)} className='control prev'><img src={iconPrev} alt='Prev button' /></span>
+            <span onClick={() => handlePage(1)} className='control next'><img src={iconNext} alt='Next button' /></span>
+          </div>
+          <div className='image'>
+            <AnimatePresence initial={false} custom={direction}>
+              <motion.img 
+                key={page}
+                custom={direction}
+                src={Items[imageIndex].image} 
+                alt={Items[imageIndex].source} 
+                initial={{x: direction > 0 ? 300 : -300, opacity: 0}}
+                exit={{zIndex: 0, x: direction < 0 ? 300 : -300, opacity: 0}}
+                animate={{zIndex: 1, x: 0, opacity: 1}}
+                transition={{
+                  x: { type: "spring", stiffness: 300, damping: 200 },
+                  opacity: { duration: 0.2 }
+                }}
+              />
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+      </div>
   );
 
 }
---
+++ src/Items.js
@@ -0,0 +1,19 @@
+import Image1 from './images/image-tanya.jpg'
+import Image2 from './images/image-john.jpg'
+
+const items = [
+{
+  image:Image1, 
+  text:"“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
+  source:"Tanya Sinclair",
+  role:"UX Engineer"
+},
+{
+  image:Image2, 
+  text:"“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+  source:"John Tarkpor",
+  role:"Junior Front-end Developer"
+}
+]
+
+export default items
